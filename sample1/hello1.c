faf353d797bc9b11177e9ce4e92c7343d576f256
Akshay <akky@gmail.com>
2015-04-06 14:40:52 +0530

    Test Purpose

---
+++ sample1/hello1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i=0;
+	
+	for (i=0; i<10; i++)
+	{
+		printf("Welcome to our world!\n");
+	}
+
+//	system("/bin/sh");
+//	system("pwd");
+	return 0;
+}
